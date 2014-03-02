1b58274e80e4a8b14f7dad097ca72e0782ef8749
haydenbbickerton <haydenbbickerton@gmail.com>
2014-03-02 14:42:39 -0600

    change fingerprint

--- cm.mk
+++ cm.mk
@@ -1,10 +1,16 @@
-$(call inherit-product, device/htc/m7spr/full_m7spr.mk)
+$(call inherit-product, device/htc/zaracl/full_zaracl.mk)
+
+# $(call inherit-product, vendor/cm/config/cdma.mk)
 
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wls BUILD_ID=JZO54K BUILD_FINGERPRINT="htc/m7wls/m7wls:4.1.2/JZO54K/166937.7:user/release-keys" PRIVATE_BUILD_DESC="1.29.651.7 CL166937 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=htc_zaracl \
+    BUILD_ID=JDQ39 \
+    BUILD_FINGERPRINT="htc/sprint_wwe_vm/zaracl:4.2.2/JDQ39/260172.9:user/release-keys" \
+    PRIVATE_BUILD_DESC="1.10.652.9 CL260172 release-keys"
 
-PRODUCT_NAME := cm_m7spr
-PRODUCT_DEVICE := m7spr
+PRODUCT_NAME := cm_zaracl
+PRODUCT_DEVICE := zaracl
